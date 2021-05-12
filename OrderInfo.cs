53fdf59aad9cd325cb0a68120ee6aa5525213ca8
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2021-05-13 01:15:20 +0530

    Sample added
    Sample added

---
+++ OrderInfo.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+
+namespace SfDataGridDemo
+{
+    public class OrderInfo : INotifyPropertyChanged
+    {
+        int orderID;
+        string customerId;
+        string country;
+        string customerName;
+        string shippingCity;
+        List<ProductInfo> productDetails;
+
+        public int OrderID
+        {
+            get { return orderID; }
+            set
+            {
+                orderID = value;
+                OnPropertyChanged("OrderID");
+            }
+        }
+
+        public string CustomerID
+        {
+            get { return customerId; }
+            set
+            {
+                customerId = value;
+                OnPropertyChanged("CustomerID");
+            }
+        }
+
+        public string CustomerName
+        {
+            get { return customerName; }
+            set
+            {
+                customerName = value;
+                OnPropertyChanged("CustomerName");
+            }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                country = value;
+                OnPropertyChanged("Country");
+            }
+        }
+
+        public string ShipCity
+        {
+            get { return shippingCity; }
+            set
+            {
+                shippingCity = value;
+                OnPropertyChanged("ShipCity");
+            }
+        }
+
+        public List<ProductInfo> ProductDetails
+        {
+            get { return productDetails; }
+            set
+            {
+                productDetails = value;
+                OnPropertyChanged("ProductDetails");
+            }
+        }
+
+        public OrderInfo()
+        {
+
+        }
+        public OrderInfo(int orderId, string customerName, string country, string customerId, string shipCity, List<ProductInfo> productdetails)
+        {
+            this.OrderID = orderId;
+            this.CustomerName = customerName;
+            this.Country = country;
+            this.CustomerID = customerId;
+            this.ShipCity = shipCity;
+            this.ProductDetails = productdetails;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(String name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+    public class ProductInfo : INotifyPropertyChanged
+    {
+        int orderId;
+        string productName;
+
+        public int OrderID
+        {
+            get { return orderId; }
+            set
+            {
+                orderId = value;
+                OnPropertyChanged("OrderID");
+            }
+        }
+
+        public string ProductName
+        {
+            get { return productName; }
+            set
+            {
+                productName = value;
+                OnPropertyChanged("ProductName");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(String name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SfDataGridDemo
+{
+    public class ViewModel
+    {
+        ObservableCollection<OrderInfo> orders;
+        public ObservableCollection<OrderInfo> Orders
+        {
+            get { return orders; }
+            set { orders = value; }
+        }
+
+        public ViewModel()
+        {
+            orders = new ObservableCollection<OrderInfo>();
+            Orders = GenerateOrders();
+        }
+
+        public ObservableCollection<OrderInfo> GenerateOrders()
+        {
+            orders.Add(new OrderInfo(1, "Maria Anders", "Germany", "ALFKI", "Berlin", getorder(1001)));
+            orders.Add(new OrderInfo(2, "Ana Trujilo", "Mexico", "ANATR", "México D.F.", getorder(1002)));
+            orders.Add(new OrderInfo(3, "Antonio Moreno", "Mexico", "ANTON", "México D.F.", getorder(1003)));
+            orders.Add(new OrderInfo(4, "Thomas Hardy", "UK", "AROUT", "London", getorder(1004)));
+            orders.Add(new OrderInfo(5, "Christina Berglund", "Sweden", "BERGS", "Luleå", getorder(1005)));
+            orders.Add(new OrderInfo(6, "Hanna Moos", "Germany", "BLAUS", "Mannheim", getorder(1006)));
+            orders.Add(new OrderInfo(7, "Frédérique Citeaux", "France", "BLONP", "Strasbourg", getorder(1007)));
+            orders.Add(new OrderInfo(8, "Martin Sommer", "Spain", "BOLID", "Madrid", getorder(1008)));
+            orders.Add(new OrderInfo(9, "Laurence Lebihan", "France", "BONAP", "Marseille", getorder(1009)));
+            orders.Add(new OrderInfo(10, "Elizabeth Lincoln", "Canada", "BOTTM", "Tsawassen", getorder(1010)));
+            orders.Add(new OrderInfo(11, "Antonio Moreno", "Mexico", "ANTON", "México D.F.", getorder(1011)));
+            orders.Add(new OrderInfo(12, "Thomas Hardy", "UK", "AROUT", "London", getorder(1012)));
+            orders.Add(new OrderInfo(13, "Christina Berglund", "Sweden", "BERGS", "Luleå", getorder(1013)));
+            orders.Add(new OrderInfo(14, "Hanna Moos", "Germany", "BLAUS", "Mannheim", getorder(1014)));
+            orders.Add(new OrderInfo(15, "Frédérique Citeaux", "France", "BLONP", "Strasbourg", getorder(1015)));
+
+            return orders;
+        }
+
+        List<ProductInfo> products = new List<ProductInfo>();
+
+        public List<ProductInfo> getorder(int i)
+        {
+            List<ProductInfo> product = new List<ProductInfo>();
+            foreach (var or in products)
+                if (or.OrderID == i)
+                    product.Add(or);
+            return product;
+        }
+    }
+}
